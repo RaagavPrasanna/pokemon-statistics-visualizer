96e7cf1c0ba34917a311e7d0cc95a389e2b58ac9
Raagav Prasanna <raagav.pras@gmail.com>
2022-12-05 21:01:21 -0500

    Label axis

--- app/src/PokemonComparisonChart.jsx
+++ app/src/PokemonComparisonChart.jsx
@@ -77,7 +77,7 @@ export default function PokemonComparisonChart(props) {
             name: props.getSelectedPokemon()
           } 
         ]}
-        layout={{width: 500, height: 500, title: 'Compared to all other pokemon'}}
+        layout={{width: 500, height: 500, title: 'Compared to all other pokemon', xaxis : {title: {text: 'Pokedex Number'}}, yaxis : {title: {text: `${props.getSelectedStat()}`}}}}
       />
     </div>
   )

180909d7ceb31a1a185bf80600f73fb9c955954c
Aidan <aidanboch@gmail.com>
2022-12-05 20:22:05 -0500

    Change port from 3000 to 5000

--- server/app.mjs
+++ server/app.mjs
@@ -7,13 +7,12 @@ import express from "express";
 import { fetchPokemonList, fetchPokemonDetails } from "./pokemonAPI.mjs";
 import {connectDB, populateDB} from "./utils/seed.mjs";
 import DB from "./db.mjs";
-const port = 3000;
+const port = 5000;
 const app = express();
 const db = new DB();
 let list = await fetchPokemonList()
 app.use(express.static('public'));
-connectDB("PokeDB", "Collection")
-//populateDB()
+connectDB("PokeDB", "Collection");
 
 app.get('/pokemonList', async (req, res) => {
     res.json(await db.readAllNames());

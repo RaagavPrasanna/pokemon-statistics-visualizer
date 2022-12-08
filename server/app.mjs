7086fff010c2171e917bf7146b6a286324cdd9ad
Raagav Prasanna <raagav.pras@gmail.com>
2022-12-08 17:15:45 -0500

    fix eslint issues

--- server/app.mjs
+++ server/app.mjs
@@ -4,7 +4,6 @@
     @date Nov 23, 2022
 */
 import express from "express";
-import { fetchPokemonList, fetchPokemonDetails } from "./pokemonAPI.mjs";
 import {connectDB, populateDB} from "./utils/seed.mjs";
 import DB from "./db.mjs";
 import cors from "cors"
@@ -20,23 +19,23 @@ await connectDB("Pokedb", "pokemon")
 // await connectDB("pokemondb", "test");
 
 if((await db.readAllNames()).length == 0) {
-    console.log("DB empty so populating db")
-    await populateDB();
+  console.log("DB empty so populating db")
+  await populateDB();
 }
 
 
 app.get('/pokemonList', async (req, res) => {
-    res.json(await db.readAllNames());
+  res.json(await db.readAllNames());
 })
 
 app.get('/pokemonDetails', async (req, res) => {
-    res.json(await db.readAll());
+  res.json(await db.readAll());
 })
 
-app.get('/pokemon/:name', async (req,res) =>{
-    let name = req.params.name
-    console.log(name)
-    res.json(await db.readOneEntry(name));
+app.get('/pokemon/:name', async (req, res) =>{
+  let name = req.params.name
+  console.log(name)
+  res.json(await db.readOneEntry(name));
 })
 // app.get('/populate', async (req,res) =>{
 //     populateDB()
